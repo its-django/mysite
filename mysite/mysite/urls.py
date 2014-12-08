d92a935ea9bd37f930bb9b3bb2a23960fbe70084
myyang <ymy1019@gmail.com>
2014-12-08 21:22:11 +0800

    Module_05 - 網址計算機
    1. 新增正規表達式匹配網址
    2. 新增數學計算視圖

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from views import here
+from views import here, math
 
 
 urlpatterns = patterns('',
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^here/$', here),
+    url(r'^(\d{1,2})/math/(\d{1,2})/$', math),
 )
