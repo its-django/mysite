e20a6c3420f09f279361309526fe551543537565
myyang <ymy1019@gmail.com>
2014-12-10 18:54:53 +0800

    Module_08 - 在restaurant使用模型
    1. 在view中使用模型讀取資料
    2. 在模版中使用模型呈現資料

--- mysite/restaurants/views.py
+++ mysite/restaurants/views.py
@@ -3,6 +3,8 @@
 
 from django.shortcuts import render_to_response
 
+from restaurants.models import Restaurant
+
 
 def menu(request):
     """retrun a menu response
@@ -11,7 +13,5 @@ def menu(request):
     :returns: http response
 
     """
-    food1 = {'name': '番茄炒蛋', 'price': 60, 'comment': '好吃', 'is_spicy': False}
-    food2 = {'name': '蒜泥白肉', 'price': 100, 'comment': '人氣推薦', 'is_spicy': True}
-    foods = [food1, food2]
+    restaurants = Restaurant.objects.all()
     return render_to_response('menu.html', locals())
