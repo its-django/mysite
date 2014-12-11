36398c539daeff58b5c5bec8a2ce858e65a3a004
myyang <ymy1019@gmail.com>
2014-12-12 00:24:53 +0800

    Module_09 - 使用ModelAdmin管理顯示欄位

--- mysite/restaurants/admin.py
+++ mysite/restaurants/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from restaurants.models import Restaurant, Food
 
-admin.site.register(Restaurant)
-admin.site.register(Food)
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'address')
+
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'restaurant', 'price')
+
+
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Food, FoodAdmin)
