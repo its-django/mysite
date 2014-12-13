10f4878795e464031dda166a803e116626bda6a2
myyang <ymy1019@gmail.com>
2014-12-13 16:53:00 +0800

    Module_10 - 選擇餐廳並觀看menu
    * 這裡使用方法一，其餘請自行修改

--- mysite/restaurants/views.py
+++ mysite/restaurants/views.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 
 from restaurants.models import Restaurant
@@ -13,8 +14,11 @@ def menu(request):
     :returns: http response
 
     """
-    restaurant = Restaurant.objects.get(id=1)
-    return render_to_response('menu.html', locals())
+    if 'id' in request.GET and request.GET['id'] != '':
+        restaurant = Restaurant.objects.get(id=request.GET['id'])
+        return render_to_response('menu.html', locals())
+    else:
+        return HttpResponseRedirect("/restaurants_list/")
 
 
 def list_restaurants(request):
