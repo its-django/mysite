3aac9928854bf864277bb6a03906fd7d59f4e614
myyang <ymy1019@gmail.com>
2014-12-14 20:37:07 +0800

    Module_11 - 自定義驗證規則

--- mysite/restaurants/forms.py
+++ mysite/restaurants/forms.py
@@ -11,3 +11,13 @@ class CommentForm(forms.Form):
     visitor = forms.CharField(max_length=20)
     email = forms.EmailField(max_length=20, required=False)
     content = forms.CharField(max_length=200, widget=forms.Textarea())
+
+    def clean_content(self):
+        """check content if len > 5
+        :returns: cleaned content
+
+        """
+        content = self.cleaned_data.get('content', '')
+        if len(content) < 5:
+            raise forms.ValidationError('字數不足')
+        return content
