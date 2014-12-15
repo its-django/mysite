51c05d646acd95c006652411a4d5aeab1eaed40a
myyang <ymy1019@gmail.com>
2014-12-15 23:01:45 +0800

    Module_14 - 匿名用戶 vs. 具名用戶
    1. 在視圖函式加上login_required裝飾器，將未登入使用者導至登入頁
    2. 在模版中加入指示登入後導回原本頁面的{{ next }}

--- mysite/restaurants/views.py
+++ mysite/restaurants/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 from restaurants.models import Restaurant, Comment
 from restaurants.forms import CommentForm
@@ -24,6 +25,7 @@ def menu(request):
         return HttpResponseRedirect("/restaurants_list/")
 
 
+@login_required
 def list_restaurants(request):
     """retrun restaurant list
 
