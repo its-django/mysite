a2139895e116fc3092f076f22e6a63edaec199dd
myyang <ymy1019@gmail.com>
2014-12-21 00:59:45 +0800

    Module_16 - 修改匯入方法簡單整理urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,8 +4,8 @@ admin.autodiscover()
 
 from django.contrib.auth.views import login, logout
 
-from views import here, math, welcome, index, register
-from restaurants.views import menu, list_restaurants, comment
+import views
+import restaurants.views
 
 
 urlpatterns = patterns('',
@@ -13,15 +13,17 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url('^index/$', index),
     url(r'^accounts/login/$', login),
     url(r'^accounts/logout/$', logout),
-    url(r'^accounts/register/$', register),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^here/$', here),
-    url(r'^(\d{1,2})/math/(\d{1,2})/$', math),
-    url(r'^menu/$', menu),
-    url(r'^welcome/$', welcome),
-    url(r'^restaurants_list/$', list_restaurants),
-    url(r'^comment/(\d{1,5})/$', comment),
+
+    url('^index/$', views.index),
+    url(r'^accounts/register/$', views.register),
+    url(r'^here/$', views.here),
+    url(r'^(\d{1,2})/math/(\d{1,2})/$', views.math),
+    url(r'^welcome/$', views.welcome),
+
+    url(r'^menu/$', restaurants.views.menu),
+    url(r'^restaurants_list/$', restaurants.views.list_restaurants),
+    url(r'^comment/(\d{1,5})/$', restaurants.views.comment),
 )
