2765b477e13fbe7d1d872decdb54a0a4397e48f6
myyang <ymy1019@gmail.com>
2014-12-23 22:30:56 +0800

    Moduel_19 - 登入、登出訪問測試
    1. 使用django內建機制
    2. 使用http協定自行發送登入/出資料

--- mysite/mysite/tests.py
+++ mysite/mysite/tests.py
@@ -3,6 +3,7 @@
 
 
 from django.test import TestCase
+from django.contrib.auth.models import User
 from django.test import client
 
 
@@ -26,3 +27,54 @@ class IndexWebpageTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, '<h2>歡迎來到餐廳王</h2>')
         self.assertTemplateUsed(resp, 'index.html')
+
+
+class LoginTestCase(TestCase):
+
+    """Login test case"""
+
+    def setUp(self):
+        """create user
+        :returns: TODO
+
+        """
+        User.objects.create_user('user', email='user@example.com', password='abcde')
+        self.c = client.Client()
+
+    def test_login_and_logout(self):
+        """test login method using django auth
+
+        """
+
+        resp = self.c.get('/restaurants_list/')
+        self.assertRedirects(resp, '/accounts/login/?next=/restaurants_list/')
+
+        self.c.login(username='user', password='abcde')
+
+        resp = self.c.get('/restaurants_list/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'restaurants_list.html')
+
+        self.c.logout()
+        resp = self.c.get('/restaurants_list/')
+        self.assertRedirects(resp, '/accounts/login/?next=/restaurants_list/')
+
+    def test_login_and_logout_by_http_protocol(self):
+        """test login method using django auth
+
+        """
+
+        resp = self.c.get('/restaurants_list/')
+        self.assertRedirects(resp, '/accounts/login/?next=/restaurants_list/')
+
+        resp = self.c.post(
+            '/accounts/login/', {'username': 'user', 'password': 'abcde'}, follow=True)
+        self.assertEqual(resp.redirect_chain, [('http://testserver/index/', 302)])
+
+        resp = self.c.get('/restaurants_list/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'restaurants_list.html')
+
+        self.c.get('/accounts/logout/')
+        resp = self.c.get('/restaurants_list/')
+        self.assertRedirects(resp, '/accounts/login/?next=/restaurants_list/')
